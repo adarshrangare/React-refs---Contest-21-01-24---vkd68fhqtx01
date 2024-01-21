d0a70c8b59744a9525a54daf64a4f08b4bb7fa68
Adarsh Rangare <akrangare@gmail.com>
2024-01-21 17:09:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,32 @@ const App = () => {
 
 //code here 
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = ()=>{
+
+    if(email.trim().length != 0 && password.trim().length != 0 ){
+
+      setEmail(emailRef.current.value);    
+      setPassword(passwordRef.current.value);    
+      
+    }
+    
+  }
+  
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={} ref={emailRef} onChange={}/><br/>
       Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
+      <input id="inputPassword" type="text" value={password} ref={passwordRef} onChange={}/><br/>
+      <button id="submitButton" onClick={handleSubmit}>Submit</button><br/>
+      <p id="emailText">Your Email : {email}</p>
+      <p id ="passwordText">Your Password : {password}</p>
       
     </div>
   )
