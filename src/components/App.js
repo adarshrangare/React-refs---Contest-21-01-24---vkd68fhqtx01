e0bf5d8449cd1d6fc3246ff95a294f34d9b178ec
Adarsh Rangare <akrangare@gmail.com>
2024-01-21 17:35:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,48 @@
-import React,{useState,useEffect,useRef} from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import '../styles/App.css';
 const App = () => {
+  //code here
 
-//code here 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  const handleSubmit = ()=>{
-
-    if(emailRef.current.value.trim().length != 0 && passwordRef.current.value.trim().length != 0 ){
-
-      setEmail(emailRef.current.value);    
-      setPassword(passwordRef.current.value);    
-      
-    } else{
-      setEmail('');
-      setPassword('');
-      if(emailRef.current.value.trim().length == 0){
-
+  const handleSubmit = () => {
+    if (
+      emailRef.current.value.trim().length != 0 &&
+      passwordRef.current.value.trim().length != 0
+    ) {
+      setEmail(emailRef.current.value);
+      setPassword(passwordRef.current.value);
+    } else {
+      setEmail("");
+      setPassword("");
+      if (emailRef.current.value.trim().length == 0) {
         emailRef.current.focus();
-      } 
-      if(passwordRef.current.value.trim().length == 0){
-
+      } else {
         passwordRef.current.focus();
-      } 
-
-      
-
-      
+      }
     }
-    
-  }
-  
+  };
+
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text"  ref={emailRef} /><br/>
+      <input id="inputEmail" type="text" ref={emailRef} />
+      <br />
       Password
-      <input id="inputPassword" type="text" ref={passwordRef} /><br/>
-      <button id="submitButton" onClick={handleSubmit}>Submit</button><br/>
+      <input id="inputPassword" type="text" ref={passwordRef} />
+      <br />
+      <button id="submitButton" onClick={handleSubmit}>
+        Submit
+      </button>
+      <br />
       <p id="emailText">Your Email : {email}</p>
-      <p id ="passwordText">Your Password : {password}</p>
-      
+      <p id="passwordText">Your Password : {password}</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
